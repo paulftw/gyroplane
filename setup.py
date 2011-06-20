b2864f147ab0558310a08588023f16f9ef4f1081
Paul Korzhyk <paul.korzhyk@gmail.com>
2011-06-20 19:05:28 +1000

    reset

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Setup script."""
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='app',
+    version='0.1',
+    author="Paul Korzhyk",
+    author_email="paul@scifiware.com",
+    description=("app engine skeleton"),
+    license="Proprietary. This code has been stolen.",
+
+    packages=find_packages(),
+    package_dir = {'': 'app'},
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'jinja2',
+        'werkzeug',
+        'werkzeug-debugger-appengine',
+        'distribute',
+    ],
+    zip_safe=False,
+)
