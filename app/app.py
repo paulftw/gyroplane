d4b81b893024e0e0897a836d31c78d17f31b1b76
Paul Korzhyk <paul.korzhyk@gmail.com>
2011-07-10 21:03:29 +1000

    templates

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html', session=app)
