8fae0e8349689d6d35f5e8c0ec3cb83292543c0b
Paul Korzhyk <paul.korzhyk@gmail.com>
2012-10-21 17:00:18 +1100

    add pysandbox and switch to py2.7

--- app/main.py
+++ app/main.py
@@ -9,16 +9,8 @@ sys.path.insert(0, './libs')
 sys.path.insert(0, './libs.zip')
 
 
-def main():
-    from app import app
-    if app.config['DEBUG']:
-        # Run debugged app
-        app.debug=True
-    from google.appengine.ext.webapp.util import run_wsgi_app
-    run_wsgi_app(app)
-
-
-# Use App Engine app caching
-if __name__ == "__main__":
-    main()
+from app import app
+if app.config['DEBUG']:
+    # Run debugged app
+    app.debug=True
 
